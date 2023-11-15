576843926173f5011098ba244c6023dffd9330be
Mateus <teus.alves.sp@gmail.com>
2023-11-15 20:08:19 -0300

    primeira execução do servidor

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const express = require("express");
-
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send("Olá Mundo");
+})
+
+app.listen(3000, () => {
+  console.log("Servidor está rodando na porta 3000!");
+})
