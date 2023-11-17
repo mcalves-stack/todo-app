b6785bd4ecbe3bc81ffdc1b12ef3f145ca191e1f
Mateus <teus.alves.sp@gmail.com>
2023-11-17 20:21:28 -0300

    criação - primeira view do projeto

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
-    res.send("Olá Mundo")
+    res.render('home')
 })
 
 app.listen(3000, () => {
