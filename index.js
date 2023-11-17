3953345c3f07f9e84501f45b69f419ed539658f6
Mateus <teus.alves.sp@gmail.com>
2023-11-17 20:58:02 -0300

    adição dos icones de lua e de sol para troca de tema

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const app = express();
 app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
     res.render('home')
 })
