537cecbefdc86be1f655f1bf48566128f55918da
Mateus <teus.alves.sp@gmail.com>
2023-11-20 16:22:23 -0300

    filtro de tarefas completas, completo

--- index.js
+++ index.js
@@ -67,6 +67,31 @@ app.post('/criar', (req, res) => {
   })
 })
 
+app.get('/completas', (req, res) => {
+  const sql = `
+    SELECT * FROM tarefas
+    WHERE completa = 1
+  `
+
+  conexao.query(sql, (erro, dados) => {
+    if(erro) {
+      return console.log(erro)
+    }
+
+    const tarefas = dados.map((dado) => {
+      return {
+        id: dado.id,
+        descricao: dado.descricao,
+        completa: true
+      }
+    })
+
+    const quantidadeTarefas = tarefas.length
+
+    res.render('completas', {tarefas, quantidadeTarefas})
+  })
+})
+
 app.get('/ativas', (req, res) => {
   const sql = `
     SELECT * FROM tarefas
